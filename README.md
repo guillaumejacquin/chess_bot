112ece75f4ed80778cb86b5c2200f9422050fe18
Guillaume <guillaume.jacquin@epitech.eu>
2021-03-05 14:49:00 +0100

    [ADD] fini de parser l heure

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ encore un de mes 580 projets, mais bon:
 Actuellement y a pas grand chose:
 
 I use the chess.com api
+pip3 install chess.com
 
 There are 2 functions.
 The first one gives you your rank and the second gives you your last match  and result(not finished yet).
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from chessdotcom import *
-import pprint
 import requests
 
-# printer = pprint.PrettyPrinter()
+
 
 def get_player_rating(username):
     data = get_player_stats(username).json
@@ -15,6 +14,20 @@ def get_player_rating(username):
         print(f'Current: {data[category]["last"]["rating"]}')
         print(f'Best: {data[category]["best"]["rating"]}')
         print(f'Best: {data[category]["record"]}')
+        
+def parser(var):
+    test = []
+    j = 0
+    for i in var:
+        if (i.isnumeric()):
+            test.append(i)
+            j+=1
+
+    try:
+        result = str(test[0]) + str(test[1])
+    except Exception:
+        result = str(test[0])
+    return(result)
 
 def get_most_recent_game(username):
     data = get_player_game_archives(username).json
@@ -27,7 +40,6 @@ def get_most_recent_game(username):
     print(games['games'][-1]['url'])
     
     date = games['games'][-1]['pgn'].splitlines()[2] #date
-    # print(games['games'][-1]['pgn'].splitlines()) 
     player_1 =(games['games'][-1]['pgn'].splitlines()[4]) #white-player
     player_2 =(games['games'][-1]['pgn'].splitlines()[5]) #black-plater
     result = (games['games'][-1]['pgn'].splitlines()[6]) #result
@@ -40,18 +52,30 @@ def get_most_recent_game(username):
     start_game = (games['games'][-1]['pgn'].splitlines()[17]) #debut de la partie
     end_game  = (games['games'][-1]['pgn'].splitlines()[19]) #fin
 
-    total_game = "0"
+
+    start_game =start_game.split(':')
+    end_game = end_game.split(':')
+
+    start_game[0] = parser(start_game[0])
+    start_game[2] = parser(start_game[2])
+    end_game[0] = parser(end_game[0])
+    end_game[2] = parser(end_game[2])
+
+    start_game2 = int(int(start_game[0]) * 3600 + int(start_game[1]) * 60 + int(start_game[2]) * 1)
+
+    end_game2 = int(end_game[0]) * 3600 + int(end_game[1]) * 60 + int(end_game[2])
+
+    total_game = end_game2 - start_game2
+
+    hours = str(total_game // 3600)
+    minutes = str(total_game // 60)  # Truncating integer division
+    secondes = str(total_game % 60)
+
+    dureepartie = (hours + " heures " + minutes+ " minutes "+ secondes + " secondes")
     print("la derniere partie de ", username ,"entre", player_1, white_elo ,"et", player_2, black_elo, "a eu lieu ce ", date, 
-    "et a commence par une", ouverture, ". La partie a commencé a:", start_game, "et a fini a ", end_game, "\nLe resultat de celle-ci:", result_precis)
-
-   
-    #print(games['games'][-1]["pgn"])
+    "et a commence par une", ouverture, ". La partie a commencé a:", start_game,"a duré", dureepartie , "et a fini a ", end_game, "\nLe resultat de celle-ci:", result_precis)
 
 
-    # for game in games['games']:
-    #     game_url = game['url']
-    # print(game_url)
-    # printer.pprint(game)
 
 #get_player_rating('timruscica')
 get_most_recent_game('pgmendormi')
